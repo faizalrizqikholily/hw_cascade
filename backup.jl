a7469f29c65cdf17a042db512b0debb713d41425
rasyaadmk <rasyaadmaulanakhandiyas_1313621020@mhs.unj.ac.id>
2023-12-18 22:38:54 +0700

    ngide update

--- backup.jl
+++ backup.jl
@@ -46,14 +46,10 @@ end
 
 function count_accuracy(kolom, real_class, predicted_class)
     new_data = hcat(kolom, real_class, predicted_class)
-
     benar = findall(new_data[:, 2] .== new_data[:, 3])  # Extract indices where benar is true
-    salah = findall(new_data[:, 2] .!= new_data[:, 3]) #Extract indices where benar is false
-
     accuracy = length(benar) / size(new_data, 1) * 100
     accuracy = floor(accuracy)
-
-    return benar, salah, accuracy
+    return benar, accuracy
 end
 
 function update_mu(data_origin, mean_origin, real_class)
@@ -75,20 +71,25 @@ function update_mu(data_origin, mean_origin, real_class)
     predictions_kolom_3 = predict_class(data_kolom_3, mean_kolom_3)
     predictions_kolom_4 = predict_class(data_kolom_4, mean_kolom_4)
 
-    benar1, salah1, accuracy1 = count_accuracy(data_kolom_1, real_class, predictions_kolom_1)
-    benar2, salah2, accuracy2 = count_accuracy(data_kolom_2, real_class, predictions_kolom_2)
-    benar3, salah3, accuracy3 = count_accuracy(data_kolom_3, real_class, predictions_kolom_3)
-    benar4, salah4, accuracy4 = count_accuracy(data_kolom_4, real_class, predictions_kolom_4)
+    benar1, accuracy1 = count_accuracy(data_kolom_1, real_class, predictions_kolom_1)
+    benar2, accuracy2 = count_accuracy(data_kolom_2, real_class, predictions_kolom_2)
+    benar3, accuracy3 = count_accuracy(data_kolom_3, real_class, predictions_kolom_3)
+    benar4, accuracy4 = count_accuracy(data_kolom_4, real_class, predictions_kolom_4)
 
     accuracies = [accuracy1, accuracy2, accuracy3, accuracy4]
-    sorted_accuracies = sort(accuracies)
-    benar = [benar1, benar2, benar3, benar4]
-    salah = [salah1, salah2, salah3, salah4]
+    sorted_accuracies = sort(accuracies, rev=true)
 
-    sorted_indices = sortperm(sorted_accuracies, rev=true)
-    sorted_benar = benar[sorted_indices]
-    sorted_salah = salah[sorted_indices]
-    return sorted_benar, sorted_salah, accuracies
+    if sorted_accuracies[1] == accuracy1
+        selected_benar = benar1
+    elseif sorted_accuracies[1] == accuracy2
+        selected_benar = benar2
+    elseif sorted_accuracies[1] == accuracy3
+        selected_benar = benar3
+    elseif sorted_accuracies[1] == accuracy4
+        selected_benar = benar4
+    end
+
+    return selected_benar, accuracies
 end
 
 #deserilize data_9m
@@ -101,23 +102,42 @@ mean_origin = calculating_mean(data_origin)
 display(mean_origin)
 
 ############## Iterasi 1 ##################
-selected_benar1, selected_salah1, accuracies_iter1 = update_mu(data_origin, mean_origin, data_origin[:, end])
-dataformu_iter1 = data_origin[selected_benar1[1], :]
-dataforcont_iter1 = data_origin[selected_salah1[1], :]
+selected_benar1, accuracies_iter1 = update_mu(data_origin, mean_origin, data_origin[:, end])
+data_iter1 = data_origin[selected_benar1, :]
+mean_iter1 = calculating_mean(data_iter1)
 
-mean_iter1 = calculating_mean(dataformu_iter1)
+
+println("Akurasi")
+display(accuracies_iter1)
 
 println()
-display(accuracies_iter1)
+println("Update miu Iterasi 1")
 display(mean_iter1)
 
 ############## Iterasi 2 ##################
-selected_benar2, selected_salah2, accuracies_iter2 = update_mu(dataforcont_iter1, mean_iter1, dataforcont_iter1[:, end])
-dataformu_iter2 = data_origin[selected_benar2[2], :]
-dataforcont_iter2 = data_origin[selected_salah2[2], :]
-
-mean_iter2 = calculating_mean(dataformu_iter2)
+selected_benar2, accuracies_iter2 = update_mu(data_iter1, mean_iter1, data_iter1[:, end])
+data_iter2 = data_iter1[selected_benar2, :]
+mean_iter2 = calculating_mean(data_iter2)
 
 println()
-display(accuracies_iter2)
+println("Update miu Iterasi 2")
 display(mean_iter2)
+
+############## Iterasi 3 ##################
+selected_benar3, accuracies_iter3 = update_mu(data_iter2, mean_iter2, data_iter2[:, end])
+data_iter3 = data_iter2[selected_benar3, :]
+mean_iter3 = calculating_mean(data_iter3)
+
+println()
+println("Update miu Iterasi 3")
+display(mean_iter3)
+
+
+############## Iterasi 4 ##################
+selected_benar4, accuracies_iter4 = update_mu(data_iter3, mean_iter3, data_iter3[:, end])
+data_iter4 = data_iter3[selected_benar4, :]
+mean_iter4 = calculating_mean(data_iter4)
+
+println()
+println("Update miu Iterasi 4")
+display(mean_iter4)
