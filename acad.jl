5bffa16d75988c32693965ab7f410c5e37afefb0
rasyaadmk <rasyaadmaulanakhandiyas_1313621020@mhs.unj.ac.id>
2023-12-18 21:10:51 +0700

    update function 2

--- acad.jl
+++ acad.jl
@@ -51,6 +51,46 @@ function count_accuracy(kolom, predicted_class)
     return benar, accuracy
 end
 
+function update_mu(data_origin, mean_origin)
+    # Pisahkan data utuh menjadi 4 databaru berdasarkan kolom
+    data_kolom_1 = data_origin[:, 1]
+    data_kolom_2 = data_origin[:, 2]
+    data_kolom_3 = data_origin[:, 3]
+    data_kolom_4 = data_origin[:, 4]
+
+    # Pisahkan data hasil concatenated_means menjadi 4 data baru berdasarkan kolom
+    mean_kolom_1 = mean_origin[:, 1]
+    mean_kolom_2 = mean_origin[:, 2]
+    mean_kolom_3 = mean_origin[:, 3]
+    mean_kolom_4 = mean_origin[:, 4]
+
+    # Hitung prediksi kelas untuk setiap kolom
+    predictions_kolom_1 = predict_class(data_kolom_1, mean_kolom_1)
+    predictions_kolom_2 = predict_class(data_kolom_2, mean_kolom_2)
+    predictions_kolom_3 = predict_class(data_kolom_3, mean_kolom_3)
+    predictions_kolom_4 = predict_class(data_kolom_4, mean_kolom_4)
+
+    benar1, accuracy1 = count_accuracy(data_kolom_1, predictions_kolom_1)
+    benar2, accuracy2 = count_accuracy(data_kolom_2, predictions_kolom_2)
+    benar3, accuracy3 = count_accuracy(data_kolom_3, predictions_kolom_3)
+    benar4, accuracy4 = count_accuracy(data_kolom_4, predictions_kolom_4)
+
+    accuracies = [accuracy1, accuracy2, accuracy3, accuracy4]
+    sorted_accuracies = sort(accuracies, rev=true)
+
+    if sorted_accuracies[1] == accuracy1
+        selected_benar = benar1
+    elseif sorted_accuracies[1] == accuracy2
+        selected_benar = benar2
+    elseif sorted_accuracies[1] == accuracy3
+        selected_benar = benar3
+    elseif sorted_accuracies[1] == accuracy4
+        selected_benar = benar4
+    end
+
+    return selected_benar
+end
+
 #deserilize data_9m
 data_origin = deserialize("data_9m.mat")
 
@@ -59,49 +99,11 @@ data_origin = convert(Array{Float64,2}, data_origin)
 
 mean_origin = calculating_mean(data_origin)
 
-# Pisahkan data utuh menjadi 4 databaru berdasarkan kolom
-data_kolom_1 = data_origin[:, 1]
-data_kolom_2 = data_origin[:, 2]
-data_kolom_3 = data_origin[:, 3]
-data_kolom_4 = data_origin[:, 4]
-
-# Pisahkan data hasil concatenated_means menjadi 4 data baru berdasarkan kolom
-mean_kolom_1 = mean_origin[:, 1]
-mean_kolom_2 = mean_origin[:, 2]
-mean_kolom_3 = mean_origin[:, 3]
-mean_kolom_4 = mean_origin[:, 4]
-
-# Hitung prediksi kelas untuk setiap kolom
-predictions_kolom_1 = predict_class(data_kolom_1, mean_kolom_1)
-predictions_kolom_2 = predict_class(data_kolom_2, mean_kolom_2)
-predictions_kolom_3 = predict_class(data_kolom_3, mean_kolom_3)
-predictions_kolom_4 = predict_class(data_kolom_4, mean_kolom_4)
-
-benar1, accuracy1 = count_accuracy(data_kolom_1, predictions_kolom_1)
-benar2, accuracy2 = count_accuracy(data_kolom_2, predictions_kolom_2)
-benar3, accuracy3 = count_accuracy(data_kolom_3, predictions_kolom_3)
-benar4, accuracy4 = count_accuracy(data_kolom_4, predictions_kolom_4)
-
-display(hcat(accuracy1, accuracy2, accuracy3, accuracy4))
-display(size(benar3))
-
-accuracies = [accuracy1, accuracy2, accuracy3, accuracy4]
-sorted_accuracies = sort(accuracies, rev=true)
-
-if sorted_accuracies[1] == accuracy1
-    selected_benar = benar1
-elseif sorted_accuracies[1] == accuracy2
-    selected_benar = benar2
-elseif sorted_accuracies[1] == accuracy3
-    selected_benar = benar3
-elseif sorted_accuracies[1] == accuracy4
-    selected_benar = benar4
-end
+selected_benar = update_mu(data_origin, mean_origin)
 
 # selecting all rows based on selected_benar
-selected_rows_iter1 = data_origin[selected_benar, :]
+data_iter1 = data_origin[selected_benar, :]
+mean_iter1 = calculating_mean(data_iter1)
 
-selected_rows_iter1 = calculating_mean(selected_rows_iter1)
 
-display(mean_origin)
-display(selected_rows_iter1)
+
